365b7d7b3f1deaaca8178cdec4c8bf33785b3443
Valentyn Vinogradskiy <vinogradskiy.v.a@gmail.com>
2018-04-20 10:22:42 +0200

    .gitignore updated

---
+++ vika-tattoo/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './assets/scss/main.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="test">
+          <a href="#" className="test-inner">Test</a>
+      </div>
+    );
+  }
+}
+
+export default App;
