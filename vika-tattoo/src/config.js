8c3ffeb2131a31bae1a72a865569f08c3ff76113
Valentyn Vinogradskiy <vinogradskiy.v.a@gmail.com>
2018-12-06 13:09:08 +0100

    test

--- vika-tattoo/src/config.js
+++ vika-tattoo/src/config.js
@@ -1,7 +1,7 @@
 export default {
     urls: {
-        info: 'http://vinogradska.com:8080/infos',
-        images_works: 'http://vinogradska.com:8080/images/works',
-        images_sketches: 'http://vinogradska.com:8080/images/sketches',
+        info: 'http://vinogradska.com/api/infos',
+        images_works: 'http://vinogradska.com/api/images/works',
+        images_sketches: 'http://vinogradska.comimages/api/sketches',
     }
 }
